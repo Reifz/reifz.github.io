fd5507c2d1a1999b77c18a02988636df3b5c6b31
Guilherme Reif <guilhermereif10@gmail.com>
2026-01-11 01:44:30 -0300

    Ajustando Esqueleto

--- dist/js/supabase.js
+++ dist/js/supabase.js
@@ -1,7 +1,7 @@
 
 // IMPORTANTE: Substitua 'SEU_SUPABASE_URL' e 'SUA_SUPABASE_ANON_KEY' pelos seus valores reais.
-const supabaseUrl = 'https://mbspfgvptdhnrnwhwpho.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ic3BmZ3ZwdGRobnJud2h3cGhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU2MjIxMTYsImV4cCI6MjA4MTE5ODExNn0.S2WWNBKSODQtZkT84BHZNMwVmfgIFpWdWo7Ds1BzE78';
+const supabaseUrl = 'https://.supabase.co';
+const supabaseKey = '..';
 
 //cliente Supabase
 const _supabase = supabase.createClient(APP_CONFIG.SUPABASE_URL,  APP_CONFIG.SUPABASE_KEY);
